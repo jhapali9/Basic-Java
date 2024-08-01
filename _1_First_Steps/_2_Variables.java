9b0f652f54ebaca5de15fbd4d289bd96f464118d
unknown <kaflevola@gmail.com>
2024-08-01 22:00:32 +0545

    Simple Variable concept

---
+++ _1_First_Steps/_2_Variables.java
@@ -0,0 +1,15 @@
+public class _2_Variables {
+    public static void main(String[] args) {
+        // see different primitive data types
+        int num = 6;
+        System.out.println(num);
+        num = 100;
+        System.out.println(num);
+        num = 5 + 4;
+        System.out.println(num);
+        num = (4 * 4) + (5 - 2);
+        System.out.println(num);
+        // try /list command in jshell to lookup all your history
+
+    }
+}
