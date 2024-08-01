2a0001eb342de7edea64f66b2fa77b83cbc6c363
unknown <kaflevola@gmail.com>
2024-08-01 21:48:33 +0545

    simple hello world program

---
+++ _1_First_Steps/_1_HelloWorld.java
@@ -0,0 +1,9 @@
+class _1_HelloWorld {
+    public static void main(String[] args) {
+        // concept of jshell
+        // error in closing parenthesis
+        // error in string literal "" used as '' which is supported in other programming
+        // languages but not in java
+        System.out.println("Hellow world!");
+    }
+}
